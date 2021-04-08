a7ae7c2d53681aaaeb669fb60493d587d3ad9845
Timothy Hobbs <timothy@hobbs.cz>
2021-04-08 10:26:25 +0200

    Add basic electron example app

---
+++ frontend/examples/ts/ticktacktoecell.ts
@@ -0,0 +1,1 @@
+console.log("Tick tack toe cell");
---
+++ frontend/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const CompressionPlugin = require('compression-webpack-plugin');
+
+module.exports = {
+    plugins: [new CompressionPlugin()],
+    resolve: {
+        extensions: ["", ".webpack.js", ".web.js", ".js", ".ts"]
+    },
+    entry: {
+        greg: './ts/greg.ts',
+        ticktacktoe: './examples/ts/ticktacktoe.ts',
+        ticktacktoecell: './examples/ts/ticktacktoecell.ts',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                loader: 'file-loader',
+            },
+            {
+                test: /\.less$/i,
+                use: ['style-loader', 'css-loader', 'less-loader'],
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js'],
+    },
+    output: {
+        path: path.resolve(__dirname, 'build/js/dist')
+    },
+    mode: "development",
+}
