949d4787d8fd91fb5f937915d98886ba840a8d19
Timothy Hobbs <timothy@hobbs.cz>
2021-10-11 15:03:24 +0200

    Make the full screen urwid client async and correctish

--- services/tick-tack-toe/gradesta-urwid-fullscreen.py
+++ services/tick-tack-toe/gradesta-urwid-fullscreen.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 import urwid
 import zmq
+import asyncio
+import zmq.asyncio
 import argparse
 import capnp
 import level0_capnp as level0
 import level0_yaml
 import parse_address
 import yaml
+import weakref
+import gradesta_vertex
 
 parser = argparse.ArgumentParser(
     description="""
@@ -17,7 +21,7 @@ parser.add_argument("socket", metavar="socket", type=str, help="socket to connec
 
 args = parser.parse_args()
 
-context = zmq.Context()
+context = zmq.asyncio.Context()
 push_socket = context.socket(zmq.PULL)
 pull_socket = context.socket(zmq.PUSH)
 push_socket_path = args.socket+".push"
@@ -26,26 +30,28 @@ push_socket.connect(push_socket_path)
 pull_socket_path = args.socket+".pull"
 print("Connecting to", pull_socket_path)
 pull_socket.connect(pull_socket_path)
+loop=asyncio.get_event_loop()
 
-cell = None
+cid = None
+cells = {}
 mode = "yaml"
 
 def get_cell(address: level0.Address):
-    global cell
+    global cid
     m = level0.Message()
     selects = m.forService.init("select", 1)
     selects[0] = address
-    if cell:
+    if cid:
         deselects = m.forService.init("deselect", 1)
-        deselects[0] = cell.forClient.vertexes[0].instanceId
+        deselects[0] = cid
+        del cells[cid]
     pull_socket.send(m.to_bytes())
-    cell = level0.Message.from_bytes(push_socket.recv())
 
 get_cell(parse_address.parse_address("gradesta://example.com/en/tick-tack-toe/   .   .   "))
 
 
 def show_or_exit(key):
-    global mode
+    global mode, cells, cid
     if key in ('q', 'Q'):
         raise urwid.ExitMainLoop()
     if key in ("y"):
@@ -59,21 +65,69 @@ def show_or_exit(key):
         "l": 2
     }
     dim = dims.get(str(key), None)
+    cell = cells[cid]
     if dim:
         if cell:
-            for pu in cell.forClient.portUpdates:
-                if pu.direction == dim:
-                    if pu.connectedVertex.which() == "vertex":
-                        get_cell(pu.connectedVertex.vertex)
-                    elif pu.connectedVertex.which() == "symlink":
-                        get_cell(pu.connectedVertex.symlink)
-    if mode == "yaml":
-        txt.set_text(yaml.dump(level0_yaml.to_dict(cell)))
-    elif mode == "data":
-        txt.set_text(cell.forClient.dataUpdates[0].data.decode("utf8"))
+            try:
+                pu = cell.ports[dim]
+            except:
+                return
+            if pu.connectedVertex.which() == "vertex":
+                get_cell(pu.connectedVertex.vertex)
+            elif pu.connectedVertex.which() == "symlink":
+                get_cell(pu.connectedVertex.symlink)
 
+def build_widgets():
+    txt = urwid.Text("Loading...")
+    def update_cell(widget_ref):
+        widget = widget_ref()
+        if not widget:
+            return
 
-txt = urwid.Text(yaml.dump(level0_yaml.to_dict(cell)))
-fill = urwid.Filler(txt, 'top')
-loop = urwid.MainLoop(fill, unhandled_input=show_or_exit)
+        if cid is not None:
+            cell = cells[cid]
+            if mode == "yaml":
+                txt.set_text(str(cell))
+            elif mode == "data":
+                txt.set_text(cell.data.data.decode("utf8"))
+
+        msg_future = push_socket.recv()
+
+        # Schedule us to update the clock again in one second
+        def process_message(msgf):
+            global cid
+            msg = level0.Message.from_bytes(msgf.result())
+            def init_cell(cid):
+                global cells
+                if cid in cells:
+                    return cells[cid]
+                vertex = gradesta_vertex.Vertex()
+                cells[cid] = vertex
+                return vertex
+            fc = msg.forClient
+            for v in fc.vertexes:
+                cell = init_cell(v.instanceId)
+                cid = v.instanceId
+                cell.vertex = v
+            for vs in fc.vertexStates:
+                cell = init_cell(vs.instanceId)
+                cell.vertexState = cell
+            for du in fc.dataUpdates:
+                cell = init_cell(du.vertexId)
+                cell.data = du
+            for pu in fc.portUpdates:
+                cell = init_cell(pu.vertexId)
+                cell.ports[pu.direction] = pu
+            for eu in fc.encryptionUpdates:
+                cell = init_cell(eu.vertexId)
+                cell.encryption = eu
+            update_cell(widget_ref)
+
+        msg_future.add_done_callback(process_message)
+
+    update_cell(weakref.ref(txt))
+    return urwid.Filler(txt, 'top')
+
+evl = urwid.AsyncioEventLoop(loop=loop)
+loop = urwid.MainLoop(build_widgets(), unhandled_input=show_or_exit, event_loop=evl)
 loop.run()
