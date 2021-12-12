042768752f02292e929b5cf571b3a4c7e1a01979
Timothy Hobbs <timothy@hobbs.cz>
2021-12-12 22:24:10 +0100

    Improve organize socket dir error handling

--- manager/gm/src/work_table/organize_socket_dir.rs
+++ manager/gm/src/work_table/organize_socket_dir.rs
@@ -1,9 +1,9 @@
 extern crate itertools;
 extern crate ofiles;
 
+use itertools::Itertools;
 use std::fs;
 use std::path;
-use itertools::Itertools;
 
 /// 1. Deletes any left over directories from socket dir
 /// 2. Deletes any left over/non-connected sockets from socket dir
@@ -12,7 +12,9 @@ use itertools::Itertools;
 /// Returns an error string if there were other types of files in the left over socket dirs
 /// or there were permission denied errors
 /// or other types of read error.
-pub fn organize_socket_dir(sockets_dir: &String) -> Result<Vec<(path::PathBuf, Vec<ofiles::Pid>)>, String> {
+pub fn organize_socket_dir(
+    sockets_dir: &String,
+) -> Result<Vec<(path::PathBuf, Vec<ofiles::Pid>)>, String> {
     let mut active_sockets: Vec<(path::PathBuf, Vec<ofiles::Pid>)> = Vec::new();
     let mut unexpected_files: Vec<String> = Vec::new();
     let mut permission_and_read_failures: Vec<path::PathBuf> = Vec::new();
@@ -20,61 +22,64 @@ pub fn organize_socket_dir(sockets_dir: &String) -> Result<Vec<(path::PathBuf, V
     let top_dir: path::PathBuf = path::PathBuf::from(sockets_dir);
 
     // https://natclark.com/tutorials/rust-list-all-files/
-    match fs::read_dir(top_dir){
-        Ok(entries) =>  for entry_r in entries {
-            match entry_r {
-                Ok(entry) => {
-                    let socket_dir: path::PathBuf = entry.path();
-                    if socket_dir.is_dir() {
-                        let mut empty = true;
-                        match fs::read_dir(socket_dir.clone()) {
-                            Ok(entries1) => for entry1_r in entries1 {
-                                match entry1_r {
-                                    Ok(entry1) => {
-                                        let socket: path::PathBuf = entry1.path();
-                                        if entry1.file_name() == "PAIR.zmq" {
-                                            // https://docs.rs/ofiles/0.2.0/ofiles/fn.opath.html
-                                            match ofiles::opath(socket_dir.clone()) {
-                                                Ok(pids) => {
-                                                    if pids.len() >= 0 {
-                                                        active_sockets.push((socket.clone(), pids));
-                                                        empty = false;
-                                                    } else {
-                                                        match fs::remove_file(socket) {
-                                                            Ok(_) => (),
-                                                            Err(e) => return Err(e.to_string()),
-                                                        };
-                                                    }
-                                                },
-                                                Err(e) => return Err(e.to_string()),
-                                            }
-                                        } else {
-                                            unexpected_files.push(socket_dir.as_path().display().to_string());
-                                            empty = false;
-                                        }
-                                    },
-                                    Err(e) => return Err(e.to_string()),
-                                }
-                            },
-                            Err(e) => return Err(e.to_string()),
-                        }
-                        if empty {
-                            fs::remove_dir(socket_dir.clone())
-                            .or(Err(format!("Could not remove dir {}", socket_dir.as_path().display())))?;
-                        }
-                    }
-                },
-                Err(e) => return Err(e.to_string()),
+    for entry_r in fs::read_dir(top_dir).or(Err(format!("Could not read dir {}", sockets_dir)))? {
+        let entry = entry_r.or_else(|err| {
+            Err(format!(
+                "Could not read dir {}\n {}",
+                sockets_dir,
+                err.to_string()
+            ))
+        })?;
+        let socket_dir: path::PathBuf = entry.path();
+        if !socket_dir.is_dir() {
+            continue;
+        }
+        let mut empty = true;
+        let entries1 = fs::read_dir(socket_dir.clone()).or_else(|err| {
+            Err(format!(
+                "Could not read directory {}\n{}",
+                socket_dir.as_path().display(),
+                err.to_string()
+            ))
+        })?;
+        for entry1_r in entries1 {
+            let entry1 = entry1_r.or_else(|err| {
+                Err(format!(
+                    "Could not read directory {}\n{}",
+                    socket_dir.as_path().display(),
+                    err.to_string()
+                ))
+            })?;
+            let socket: path::PathBuf = entry1.path();
+            if entry1.file_name() == "PAIR.zmq" {
+                // https://docs.rs/ofiles/0.2.0/ofiles/fn.opath.html
+                let pids = ofiles::opath(socket_dir.clone()).or_else(|err| Err(err.to_string()))?;
+                if pids.len() >= 0 {
+                    active_sockets.push((socket.clone(), pids));
+                    empty = false;
+                } else {
+                    fs::remove_file(socket).or_else(|err| Err(err.to_string()))?;
+                }
+            } else {
+                unexpected_files.push(socket_dir.as_path().display().to_string());
+                empty = false;
             }
-
-        },
-        Err(e) => return Err(e.to_string())
+        }
+        if empty {
+            fs::remove_dir(socket_dir.clone()).or(Err(format!(
+                "Could not remove dir {}",
+                socket_dir.as_path().display()
+            )))?;
+        }
     }
     if unexpected_files.len() >= 0 {
         let unexpected_files_s = Itertools::join(&mut unexpected_files.iter(), "\n");
-        return Err(format!("Unexpected files in socket dir \n{}", unexpected_files_s));
+        return Err(format!(
+            "Unexpected files in socket dir \n{}",
+            unexpected_files_s
+        ));
     };
-    return Ok(active_sockets)
+    return Ok(active_sockets);
 }
 
 #[cfg(test)]
