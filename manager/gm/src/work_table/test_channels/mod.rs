3d80a25926969d617de22c476609cae75db6d739
Timothy Hobbs <timothy@hobbs.cz>
2021-12-25 23:25:29 +0100

    Use different sockets for test_channels based on thread so parallel tests work

--- manager/gm/src/work_table/test_channels/mod.rs
+++ manager/gm/src/work_table/test_channels/mod.rs
@@ -1,6 +1,3 @@
-#[cfg(test)]
-use zmq;
-
 const DEFAULT_SOCKET_PATH: &str = "/tmp/rust_test_channels_socket.ZMQ_REQ_REP";
 
 #[cfg(not(test))]
@@ -9,8 +6,9 @@ pub fn report_to_custom_socket(socket_path: &str) {}
 #[cfg(test)]
 pub fn report_to_custom_socket(message: &str, socket_path: &str) {
     use std::path;
+    use zmq;
     if !path::Path::new(socket_path).exists() {
-        return // Fall through if we're not expecting anything so that non-expectant tests work.
+        return; // Fall through if we're not expecting anything so that non-expectant tests work.
     }
 
     let ctx = zmq::Context::new();
@@ -23,64 +21,84 @@ pub fn report_to_custom_socket(message: &str, socket_path: &str) {
 
 #[cfg(test)]
 pub fn report(message: &str) {
-    report_to_custom_socket(message, DEFAULT_SOCKET_PATH);
+    report_to_custom_socket(message, &for_tests::get_default_socket_path());
 }
 
 #[cfg(not(test))]
 pub fn report(message: &str) {}
 
 #[cfg(test)]
-pub fn open_test_channel_with_custom_socket(socket_path: &str) ->  zmq::Socket {
-    let ctx = zmq::Context::new();
-    let socket = ctx.socket(zmq::REP).unwrap();
-    let socket_url = format!("ipc://{}", socket_path);
-    socket.bind(&socket_url).unwrap();
-    return socket;
-}
+pub mod for_tests {
+    use super::*;
+    use zmq;
+    use std::path;
+    use sha2::{Sha256, Digest};
+    use hex;
 
-#[cfg(test)]
-pub fn open_test_channel() -> zmq::Socket {
-    open_test_channel_with_custom_socket(DEFAULT_SOCKET_PATH)
-}
-
-pub fn expect(message: &str, socket: &zmq::Socket) {
-    loop {
-        let incoming: String = socket.recv_string(0).unwrap().unwrap();
-        if incoming == message {
-            return
-        }
-        println!("Ignoring incoming message:\n {}", incoming);
-        send_continue(socket);
+    pub fn get_default_socket_path() -> String {
+        let mut hasher = Sha256::new();
+        hasher.update(std::thread::current().name().unwrap().clone().as_bytes());
+        let thread_name_hash = hex::encode(hasher.finalize());
+        format!("{}{}",
+            DEFAULT_SOCKET_PATH,
+            thread_name_hash,
+        )
     }
-}
 
-pub fn send_continue(socket: &zmq::Socket) {
-    socket.send("", 0).unwrap();
-}
+    pub fn open_test_channel_with_custom_socket(socket_path: &str) -> zmq::Socket {
+        assert!(!path::Path::new(socket_path).exists());
+        let ctx = zmq::Context::new();
+        let socket = ctx.socket(zmq::REP).unwrap();
+        let socket_url = format!("ipc://{}", socket_path);
+        socket.bind(&socket_url).unwrap();
+        return socket;
+    }
 
-pub fn clear_expectations(socket: zmq::Socket) {
-    expect("END", &socket);
-    send_continue(&socket);
-    let url = socket.get_last_endpoint().unwrap().unwrap();
-    socket.disconnect(&url).unwrap();
-    drop(socket);
-    use std::fs;
-    fs::remove_file(&url[6..]).unwrap();
+    #[cfg(test)]
+    pub fn open_test_channel() -> zmq::Socket {
+        open_test_channel_with_custom_socket(&get_default_socket_path())
+    }
+
+    pub fn expect(message: &str, socket: &zmq::Socket) {
+        loop {
+            let incoming: String = socket.recv_string(0).unwrap().unwrap();
+            if incoming == message {
+                return;
+            }
+            println!("Ignoring incoming message:\n {}", incoming);
+            send_continue(socket);
+        }
+    }
+
+    pub fn send_continue(socket: &zmq::Socket) {
+        socket.send("", 0).unwrap();
+    }
+
+    pub fn clear_expectations(socket: zmq::Socket) {
+        expect("END", &socket);
+        send_continue(&socket);
+        let url = socket.get_last_endpoint().unwrap().unwrap();
+        socket.disconnect(&url).unwrap();
+        drop(socket);
+        use std::fs;
+        fs::remove_file(&url[6..]).unwrap();
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::for_tests::*;
     use super::*;
     const MAGIC_TEST_FILE: &str = "/tmp/rust_test_channels_magic_test_file";
 
     #[test]
     fn test_test_channels() {
-        use std::thread;
         use std::fs;
         use std::path;
-        assert!(!path::Path::new(DEFAULT_SOCKET_PATH).exists());
+        assert!(!path::Path::new(&get_default_socket_path()).exists());
         report(&"ignored");
         fs::remove_file(MAGIC_TEST_FILE).unwrap_or_default();
+        use std::thread;
         let channel = open_test_channel();
         let handle = thread::spawn(|| {
             expect(&"foo", &channel);
@@ -91,7 +109,7 @@ mod tests {
             clear_expectations(channel);
         });
         report(&"foo");
-        assert!(path::Path::new(DEFAULT_SOCKET_PATH).exists());
+        assert!(path::Path::new(&get_default_socket_path()).exists());
         report(&"baz");
         assert!(!path::Path::new(MAGIC_TEST_FILE).exists());
         report(&"bar");
@@ -99,6 +117,6 @@ mod tests {
         report(&"lol");
         report(&"END");
         handle.join().unwrap();
-        assert!(!path::Path::new(DEFAULT_SOCKET_PATH).exists());
+        assert!(!path::Path::new(&get_default_socket_path()).exists());
     }
 }
