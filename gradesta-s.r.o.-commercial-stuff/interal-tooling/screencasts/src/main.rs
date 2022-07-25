20522fdfb243558bd756afa51930aec5a333a83e
Timothy Hobbs <timothyhobbs@seznam.cz>
2022-07-25 16:31:54 +0000

    WIP: Getting very close to implementing screencast uploader

--- gradesta-s.r.o.-commercial-stuff/interal-tooling/screencasts/src/main.rs
+++ gradesta-s.r.o.-commercial-stuff/interal-tooling/screencasts/src/main.rs
@@ -1,8 +1,46 @@
 mod screencasts;
+use screencasts::transcode_and_upload::{transcode_and_upload};
+use std::fs;
+use std::process;
 
 extern crate clap;
-use clap::{App, Arg, SubCommand};
+use clap::{arg, command, Command};
 
 fn main() {
-    println!("Hello, world!");
+    let matches = command!() // requires `cargo` feature
+        .propagate_version(true)
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .subcommand(
+            Command::new("publish")
+                .about("Publishes screencasts to s3")
+                .arg(arg!(<BLOG_POST>))
+                .arg(arg!(<VIDEO_FILE>...))
+        )
+        .get_matches();
+
+    match matches.subcommand() {
+        Some(("publish", sub_matches)) => {
+            let blog_post_path: &String = sub_matches.get_one::<String>("BLOG_POST").unwrap();
+            let blog_post = fs::read_to_string(blog_post_path).unwrap();
+
+            let video_files = sub_matches.get_many::<String>("VIDEO_FILE").unwrap();
+            let video_files: Vec<&str> = video_files.map(|s| s.as_ref()).collect();
+            match transcode_and_upload(&blog_post, video_files) {
+                Ok(commands) => {
+                    for commandset in commands {
+                        for command in commandset {
+                            println!("{:?}", command);
+                        }
+                    }
+                },
+                Err(e) => {
+                    eprintln!("{}", e);
+                    process::exit(1);
+                }
+            };
+
+        },
+        _ => unreachable!("Exhausted list of subcommands and subcommand_required prevents `None`"),
+    }
 }
---
+++ gradesta-s.r.o.-commercial-stuff/interal-tooling/screencasts/src/screencasts/transcode_and_upload.rs
@@ -0,0 +1,56 @@
+use super::parsing::{extract_screencast_tags};
+use std::path::{Path};
+use std::error::{Error};
+
+/// Takes the contents of a blog post and returns a list of lists of external commands to run.
+/// These lists can be run in paralel.
+///
+pub fn transcode_and_upload<'a>(blogpost: &'a str, video_files: Vec<&str>) -> Result<Vec<Vec<Vec<String>>>, Box<dyn Error + 'a>>
+{
+    let (_, screencasts) = extract_screencast_tags(blogpost)?;
+    if video_files.len() != screencasts.len() {
+        return Err(format!("There are {} screencasts refered to in the blog post but {} were passed. Please pass one video file per screencast tag.",
+                           screencasts.len(),
+                           video_files.len()
+        ).into())
+    }
+    let mut commands = vec![];
+    let mut i = 0;
+    for screencast in screencasts {
+        let mkvfile = video_files[i];
+        let mp4file_path = Path::new(video_files[i]).with_extension("mp4");
+        let path_encoding_error: Box<dyn Error> = format!("Invalid character in video file path {}.", video_files[i]).into();
+        let mp4file = mp4file_path.to_str().ok_or(path_encoding_error)?;
+        let s3path = format!("s3://gradesta-web-static/screencasts/{}.mp4", screencast.id);
+        commands.push(
+            vec![
+                ["ffmpeg", "-i", mkvfile, mp4file].iter().map(|s| s.to_string()).collect(),
+                ["s3cmd", "put", "-P", mp4file, &s3path].iter().map(|s| s.to_string()).collect()
+            ]
+        );
+        i += 1;
+    };
+    return Ok(commands);
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_generate_commands() {
+        let commands = transcode_and_upload("My blog post {{<screencast \"video-id\">}}", vec!["/home/user/Videos/screencast1.mkv"]).unwrap();
+        assert_eq!(commands, [[
+           vec!["ffmpeg", "-i", "/home/user/Videos/screencast1.mkv", "/home/user/Videos/screencast1.mp4"],
+           vec!["s3cmd", "put", "-P", "/home/user/Videos/screencast1.mp4", "s3://gradesta-web-static/screencasts/video-id.mp4"]
+        ]]);
+
+        let commands = transcode_and_upload("My blog post {{<screencast \"video-id\">}} {{<screencast \"video-id\">}}", vec!["/home/user/Videos/screencast1.mkv"]);
+        match commands {
+            Ok(_) => assert!(false),
+            Err(e) =>
+                assert_eq!(e.to_string(), "There are 2 screencasts refered to in the blog post but 1 were passed. Please pass one video file per screencast tag.")
+        }
+    }
+}
