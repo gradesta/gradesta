f97cbf9c2056a0166d81f61d104f230aa2a6e018
Timothy Hobbs <timothy@hobbs.cz>
2022-10-23 22:48:21 +0200

    roll back bad changes to shell.nix

--- shell.nix
+++ shell.nix
@@ -9,5 +9,13 @@ mkShell {
         pkgs.llvmPackages.bintools
         cargo-watch
         cargo-tarpaulin
+        python310
+        python310Packages.black
+        python310Packages.pytest
+        python310Packages.pytest-cov
+        python310Packages.setuptools
+        python310Packages.pip
+        python310Packages.virtualenv
+        python310Packages.wheel
     ];
 }
