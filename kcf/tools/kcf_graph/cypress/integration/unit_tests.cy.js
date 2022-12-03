f8a2be0a42a5af212c360eff0f3964d2321e5ed1
Timothy Hobbs <timothyhobbs@seznam.cz>
2022-12-03 18:43:46 +0000

    kcf: kcf_graph implement get_date_range

--- kcf/tools/kcf_graph/cypress/integration/unit_tests.cy.js
+++ kcf/tools/kcf_graph/cypress/integration/unit_tests.cy.js
@@ -10,6 +10,16 @@ describe("Unit tests", function () {
     })
 
     context('kcfGraph.js', function () {
-        expect(kcfGraph.get_date_range([])).to.eq((null, null))
+        it("get_date_range doesn't crash when an empty task list is passed.", function () {
+            expect(kcfGraph.get_date_range([])).to.eq((null, null))
+        })
+
+        it("get_date_range works with valid data", function () {
+            cy.fixture('tasks').then((tasks) => {
+                expect(kcfGraph.get_date_range(tasks)).to.eq(("2022-09-01 00:00", "2022-10-23 00:00"))
+            })
+
+
+        })
     })
 })
