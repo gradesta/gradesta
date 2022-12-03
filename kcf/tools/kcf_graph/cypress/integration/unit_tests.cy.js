fd37b8b7e2b446351321a03ac6238566081922d9
Timothy Hobbs <timothyhobbs@seznam.cz>
2022-12-03 21:18:41 +0000

    kcf-graph: get_dates_as_ms/as_strings

--- kcf/tools/kcf_graph/cypress/integration/unit_tests.cy.js
+++ kcf/tools/kcf_graph/cypress/integration/unit_tests.cy.js
@@ -1,25 +1,32 @@
 /// <reference types="cypress" />
 
-import kcfGraph from "../../kcfGraph";
+import {get_date_range, get_dates_as_strings} from "../../kcfGraph";
 
 describe("Unit tests", function () {
 
     before(() => {
         // check if the import worked correctly
-        expect(kcfGraph.get_date_range, 'get_date_range').to.be.a('function')
+        expect(get_date_range, 'get_date_range').to.be.a('function')
     })
 
     context('kcfGraph.js', function () {
         it("get_date_range doesn't crash when an empty task list is passed.", function () {
-            expect(kcfGraph.get_date_range([])).to.eq((null, null))
+            expect(get_date_range([])).to.eq((null, null))
         })
 
         it("get_date_range works with valid data", function () {
             cy.fixture('tasks').then((tasks) => {
-                expect(kcfGraph.get_date_range(tasks)).to.eq(("2022-09-01 00:00", "2022-10-23 00:00"))
+                expect(get_date_range(tasks)).to.eq(("2022-09-01 00:00", "2022-10-23 00:00"))
             })
+        })
 
-
+        it("get_dates_as_strings works with valid data (simultaneously tests get_dates_as_ms)", function () {
+            cy.fixture('tasks').then((tasks) => {
+                let dates = get_dates_as_strings("2022-09-01 00:00", "2022-09-3 00:00");
+                expect(dates[0]).to.eq("2022-09-01")
+                expect(dates[1]).to.eq("2022-09-02")
+                expect(dates[2]).to.eq("2022-09-03")
+            })
         })
     })
 })
--- kcf/tools/kcf_graph/kcfGraph.js
+++ kcf/tools/kcf_graph/kcfGraph.js
@@ -1,51 +1,66 @@
-export default {
-
-    scales: {
-        x: {
-            type: 'category',
-            min: 5,
-            max: 11,
-            grid: {
-                color: 'rgba( 0, 0, 0, 0.1)',
-            },
-            title: {
-                display: true,
-                text: (ctx) => ctx.scale.axis + ' axis',
-            }
+export const scales = {
+    x: {
+        type: 'category',
+        min: 5,
+        max: 11,
+        grid: {
+            color: 'rgba( 0, 0, 0, 0.1)',
         },
-        y: {
-            min: 0,
-            max: 20,
-            grid: {
-                color: 'rgba( 0, 0, 0, 0.1)',
-            },
-            title: {
-                display: true,
-                text: (ctx) => ctx.scale.axis + ' axis',
-            }
-        },
+        title: {
+            display: true,
+            text: (ctx) => ctx.scale.axis + ' axis',
+        }
     },
+    y: {
+        min: 0,
+        max: 20,
+        grid: {
+            color: 'rgba( 0, 0, 0, 0.1)',
+        },
+        title: {
+            display: true,
+            text: (ctx) => ctx.scale.axis + ' axis',
+        }
+    },
+}
 
-    get_date_range: (tasks) => {
-        let earliest_relevant_date = null;
-        let latest_relevant_date = null;
-        for (const task of tasks) {
-            if (earliest_relevant_date === null) {
-                earliest_relevant_date = task.CREATED;
+export function get_date_range(tasks) {
+    let earliest_relevant_date = null;
+    let latest_relevant_date = null;
+    for (const task of tasks) {
+        if (earliest_relevant_date === null) {
+            earliest_relevant_date = task.CREATED;
+        }
+        if (task.COMPLETED) {
+            if (latest_relevant_date === null) {
+                latest_relevant_date = task.COMPLETED;
             }
-            if (task.COMPLETED) {
-                if (latest_relevant_date === null) {
-                    latest_relevant_date = task.COMPLETED;
-                }
-                if (task.COMPLETED > latest_relevant_date) {
-                    latest_relevant_date = task.COMPLETED;
-                }
-            }
-            if (task.CREATED < earliest_relevant_date) {
-                earliest_relevant_date = task.CREATED;
+            if (task.COMPLETED > latest_relevant_date) {
+                latest_relevant_date = task.COMPLETED;
             }
         }
-
-        return (earliest_relevant_date, latest_relevant_date)
+        if (task.CREATED < earliest_relevant_date) {
+            earliest_relevant_date = task.CREATED;
+        }
     }
+
+    return (earliest_relevant_date, latest_relevant_date)
+}
+
+export function get_dates_as_ms (earliest_relevant_date, latest_relevant_date) {
+    let dates = []
+    let day = Date.parse(earliest_relevant_date.split(" ")[0]);
+    let latest_day = Date.parse(latest_relevant_date.split(" ")[0])
+    while (day <= latest_day) {
+        dates.push(day);
+        day += 1000*60*60*24;
+    }
+    return dates
+}
+
+export function get_dates_as_strings(earliest_relevant_date, latest_relevant_date) {
+    return get_dates_as_ms(earliest_relevant_date, latest_relevant_date).map(date => {
+        let d = new Date(date);
+        return d.getFullYear() + "-" + ("0"+(d.getMonth()+1)).slice(-2) + "-" + ("0" + d.getDate()).slice(-2);
+    });
 }
