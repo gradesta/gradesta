82932cffe723d59ee6a68f4f9bad990311952064
Timothy Hobbs <timothyhobbs@seznam.cz>
2022-12-04 01:38:05 +0000

    kcf_graph: install eslint and lint code

--- kcf/tools/kcf_graph/cypress/integration/unit_tests.cy.js
+++ kcf/tools/kcf_graph/cypress/integration/unit_tests.cy.js
@@ -1,51 +1,49 @@
 /// <reference types="cypress" />
 
-import {get_date_range, get_dates_as_strings, get_estimates_hours} from "../../kcfGraph";
+import { get_date_range, get_dates_as_strings, get_estimates_hours } from '../../kcfGraph'
 
-describe("Unit tests", function () {
+describe('Unit tests', function () {
+  before(() => {
+    // check if the import worked correctly
+    expect(get_date_range, 'get_date_range').to.be.a('function')
+  })
 
-    before(() => {
-        // check if the import worked correctly
-        expect(get_date_range, 'get_date_range').to.be.a('function')
+  context('kcfGraph.js', function () {
+    it("get_date_range doesn't crash when an empty task list is passed.", function () {
+      expect(get_date_range([]).start).to.eq((null))
+      expect(get_date_range([]).end).to.eq((null))
     })
 
-    context('kcfGraph.js', function () {
-        it("get_date_range doesn't crash when an empty task list is passed.", function () {
-            expect(get_date_range([]).start).to.eq((null));
-            expect(get_date_range([]).end).to.eq((null));
-        })
-
-        it("get_date_range works with valid data", function () {
-            cy.fixture('tasks').then((tasks) => {
-                let range = get_date_range(tasks);
-                expect(range.start).to.eq("2022-09-01 18:49");
-                expect(range.end).to.eq("2022-10-23 00:00");
-            })
-        })
-
-        it("get_dates_as_strings works with valid data (simultaneously tests get_dates_as_ms)", function () {
-            let dates = get_dates_as_strings("2022-09-01 00:00", "2022-09-3 00:00");
-            expect(dates[0]).to.eq("2022-09-01");
-            expect(dates[1]).to.eq("2022-09-02");
-            expect(dates[2]).to.eq("2022-09-03");
-            expect(dates.length).to.eq(3);
-        });
-
-
-        it("get_estimates_hours works with valid data", function () {
-            cy.fixture('tasks').then((tasks) => {
-                expect(tasks.length).to.eq(11);
-                let range = get_date_range(tasks);
-                expect(range.start).to.eq("2022-09-01 18:49");
-                expect(range.end).to.eq("2022-10-23 00:00");
-                let estimated_hours = get_estimates_hours(tasks, get_dates_as_strings(range.start, range.end));
-                expect(estimated_hours.min[0]).to.eq(0);
-                expect(estimated_hours.min[1]).to.eq(4);
-                expect(estimated_hours.max[0]).to.eq(0);
-                expect(estimated_hours.max[1]).to.eq(44);
-                expect(estimated_hours.min.length).to.eq(estimated_hours.max.length)
-                expect(estimated_hours.min.length).to.eq(53)
-            })
-        })
+    it('get_date_range works with valid data', function () {
+      cy.fixture('tasks').then((tasks) => {
+        const range = get_date_range(tasks)
+        expect(range.start).to.eq('2022-09-01 18:49')
+        expect(range.end).to.eq('2022-10-23 00:00')
+      })
     })
+
+    it('get_dates_as_strings works with valid data (simultaneously tests get_dates_as_ms)', function () {
+      const dates = get_dates_as_strings('2022-09-01 00:00', '2022-09-3 00:00')
+      expect(dates[0]).to.eq('2022-09-01')
+      expect(dates[1]).to.eq('2022-09-02')
+      expect(dates[2]).to.eq('2022-09-03')
+      expect(dates.length).to.eq(3)
+    })
+
+    it('get_estimates_hours works with valid data', function () {
+      cy.fixture('tasks').then((tasks) => {
+        expect(tasks.length).to.eq(11)
+        const range = get_date_range(tasks)
+        expect(range.start).to.eq('2022-09-01 18:49')
+        expect(range.end).to.eq('2022-10-23 00:00')
+        const estimated_hours = get_estimates_hours(tasks, get_dates_as_strings(range.start, range.end))
+        expect(estimated_hours.min[0]).to.eq(0)
+        expect(estimated_hours.min[1]).to.eq(4)
+        expect(estimated_hours.max[0]).to.eq(0)
+        expect(estimated_hours.max[1]).to.eq(44)
+        expect(estimated_hours.min.length).to.eq(estimated_hours.max.length)
+        expect(estimated_hours.min.length).to.eq(53)
+      })
+    })
+  })
 })
--- kcf/tools/kcf_graph/kcfGraph.js
+++ kcf/tools/kcf_graph/kcfGraph.js
@@ -1,119 +1,121 @@
 export const scales = {
-    x: {
-        type: 'category',
-        min: 5,
-        max: 11,
-        grid: {
-            color: 'rgba( 0, 0, 0, 0.1)',
-        },
-        title: {
-            display: true,
-            text: (ctx) => ctx.scale.axis + ' axis',
-        }
+  x: {
+    type: 'category',
+    min: 5,
+    max: 11,
+    grid: {
+      color: 'rgba( 0, 0, 0, 0.1)'
     },
-    y: {
-        min: 0,
-        max: 20,
-        grid: {
-            color: 'rgba( 0, 0, 0, 0.1)',
-        },
-        title: {
-            display: true,
-            text: (ctx) => ctx.scale.axis + ' axis',
-        }
+    title: {
+      display: true,
+      text: (ctx) => ctx.scale.axis + ' axis'
+    }
+  },
+  y: {
+    min: 0,
+    max: 20,
+    grid: {
+      color: 'rgba( 0, 0, 0, 0.1)'
     },
+    title: {
+      display: true,
+      text: (ctx) => ctx.scale.axis + ' axis'
+    }
+  }
 }
 
-export function get_date_range(tasks) {
-    let earliest_relevant_date = null;
-    let latest_relevant_date = null;
-    for (const task of tasks) {
-        if (earliest_relevant_date === null) {
-            earliest_relevant_date = task.CREATED;
-        }
-        if (task.COMPLETED) {
-            if (latest_relevant_date === null) {
-                latest_relevant_date = task.COMPLETED;
-            }
-            if (task.COMPLETED > latest_relevant_date) {
-                latest_relevant_date = task.COMPLETED;
-            }
-        }
-        if (task.CREATED < earliest_relevant_date) {
-            earliest_relevant_date = task.CREATED;
-        }
+export function get_date_range (tasks) {
+  let earliest_relevant_date = null
+  let latest_relevant_date = null
+  for (const task of tasks) {
+    if (earliest_relevant_date === null) {
+      earliest_relevant_date = task.CREATED
     }
+    if (task.COMPLETED) {
+      if (latest_relevant_date === null) {
+        latest_relevant_date = task.COMPLETED
+      }
+      if (task.COMPLETED > latest_relevant_date) {
+        latest_relevant_date = task.COMPLETED
+      }
+    }
+    if (task.CREATED < earliest_relevant_date) {
+      earliest_relevant_date = task.CREATED
+    }
+  }
 
-    return {start: earliest_relevant_date, end: latest_relevant_date}
+  return { start: earliest_relevant_date, end: latest_relevant_date }
 }
 
 export function get_dates_as_ms (earliest_relevant_date, latest_relevant_date) {
-    let dates = []
-    let day = Date.parse(earliest_relevant_date.split(" ")[0]);
-    let latest_day = Date.parse(latest_relevant_date.split(" ")[0])
-    while (day <= latest_day) {
-        dates.push(day);
-        day += 1000*60*60*24;
-    }
-    return dates
+  const dates = []
+  let day = Date.parse(earliest_relevant_date.split(' ')[0])
+  const latest_day = Date.parse(latest_relevant_date.split(' ')[0])
+  while (day <= latest_day) {
+    dates.push(day)
+    day += 1000 * 60 * 60 * 24
+  }
+  return dates
 }
 
-export function get_dates_as_strings(earliest_relevant_date, latest_relevant_date) {
-    return get_dates_as_ms(earliest_relevant_date, latest_relevant_date).map(date => {
-        let d = new Date(date);
-        return d.getFullYear() + "-" + ("0"+(d.getMonth()+1)).slice(-2) + "-" + ("0" + d.getDate()).slice(-2);
-    });
+export function get_dates_as_strings (earliest_relevant_date, latest_relevant_date) {
+  return get_dates_as_ms(earliest_relevant_date, latest_relevant_date).map(date => {
+    const d = new Date(date)
+    return d.getFullYear() + '-' + ('0' + (d.getMonth() + 1)).slice(-2) + '-' + ('0' + d.getDate()).slice(-2)
+  })
 }
 
-export function get_estimates_hours(tasks, dates) {
-    let min_estimates_hours = [];
-    let max_estimates_hours = [];
-    for (const date of dates) {
-        let min_estimate_sec = 0;
-        let max_estimate_sec = 0;
-        for (const task of tasks) {
-            if (task.CREATED <= date) {
-                if (task.COMPLETED <= date) {
-                    continue;
-                }
-                min_estimate_sec += task.TIME_COST_ESTIMATES_SUMMARY.individual_work_min;
-                max_estimate_sec += task.TIME_COST_ESTIMATES_SUMMARY.individual_work_max;
-            }
+export function get_estimates_hours (tasks, dates) {
+  const min_estimates_hours = []
+  const max_estimates_hours = []
+  for (const date of dates) {
+    let min_estimate_sec = 0
+    let max_estimate_sec = 0
+    for (const task of tasks) {
+      if (task.CREATED <= date) {
+        if (task.COMPLETED <= date) {
+          continue
         }
-        min_estimates_hours.push(min_estimate_sec / 60 / 60);
-        max_estimates_hours.push(max_estimate_sec / 60 / 60);
+        min_estimate_sec += task.TIME_COST_ESTIMATES_SUMMARY.individual_work_min
+        max_estimate_sec += task.TIME_COST_ESTIMATES_SUMMARY.individual_work_max
+      }
     }
-    return {min: min_estimates_hours, max: max_estimates_hours}
+    min_estimates_hours.push(min_estimate_sec / 60 / 60)
+    max_estimates_hours.push(max_estimate_sec / 60 / 60)
+  }
+  return { min: min_estimates_hours, max: max_estimates_hours }
 }
 
-export function estimated_time_remaining(element, tasks) {
-    let date_range = get_date_range(tasks);
-    let labels = get_dates_as_strings(date_range.start, date_range.end);
-    let estimated_hours = get_estimates_hours(tasks, labels);
-    new Chart(element, {
-        type: 'line',
-        labels: labels,
-        data: {
-            datasets: [
-                {
-                    label: "min estimated hours",
-                    data: estimated_hours.min,
-                },
-                {
-                    label: "max estimated hours",
-                    data: estimated_hours.max,
-                },
-            ]
+export function estimated_time_remaining (element, tasks) {
+  const date_range = get_date_range(tasks)
+  const labels = get_dates_as_strings(date_range.start, date_range.end)
+  const estimated_hours = get_estimates_hours(tasks, labels)
+  new Chart(element, {
+    type: 'line',
+    labels,
+    data: {
+      datasets: [
+        {
+          label: 'min estimated hours',
+          data: estimated_hours.min
         },
-        options: {
-            scales: scales,
-            plugins: {
-                zoom: {
-                    pan: {
-                        enabled: true,
-                        threshold: 14,
-                        mode: "x",
-                    }
-                }}}
-    });
+        {
+          label: 'max estimated hours',
+          data: estimated_hours.max
+        }
+      ]
+    },
+    options: {
+      scales,
+      plugins: {
+        zoom: {
+          pan: {
+            enabled: true,
+            threshold: 14,
+            mode: 'x'
+          }
+        }
+      }
+    }
+  })
 }
